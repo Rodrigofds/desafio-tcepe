306f701ab9cecfd23c2a716502d674eef5a5f3f4
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2024-11-12 19:21:33 -0300

    Commit inicial

---
+++ src/main/java/com/pitang/desafio_tcepe/DesafioTcepeApplication.java
@@ -0,0 +1,13 @@
+package com.pitang.desafio_tcepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTcepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTcepeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitang/desafio_tcepe/DesafioTcepeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitang.desafio_tcepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTcepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
