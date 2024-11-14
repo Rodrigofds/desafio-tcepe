57e30637b99c9f5b5b883b0263008b5eba268375
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2024-11-13 23:14:33 -0300

    [USER002] feature - Implementa testes de unidade para método createUser da User Service

--- src/test/java/com/pitang/desafio/tcepe/service/UserServiceImplTest.java
+++ src/test/java/com/pitang/desafio/tcepe/service/UserServiceImplTest.java
@@ -2,6 +2,8 @@ package com.pitang.desafio.tcepe.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pitang.desafio.tcepe.dto.UserDTO;
+import com.pitang.desafio.tcepe.exception.expections.EmailException;
+import com.pitang.desafio.tcepe.exception.expections.LoginException;
 import com.pitang.desafio.tcepe.model.Car;
 import com.pitang.desafio.tcepe.model.User;
 import com.pitang.desafio.tcepe.repository.IUserRepository;
@@ -17,9 +19,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 class UserServiceImplTest {
@@ -31,13 +39,13 @@ class UserServiceImplTest {
     private UserServiceImpl userService;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws ParseException {
         MockitoAnnotations.openMocks(this);
         ObjectMapper objectMapper = new ObjectMapper();
     }
 
     @Test
-    void deveDeveRetornarListaUsuariosVazia() {
+    void shouldReturnAnEmptyList() {
         when(userRepository.findAll()).thenReturn(Collections.emptyList());
 
         List<UserDTO> result = userService.findAllUsers();
@@ -46,7 +54,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void deveDeveRetornarListaUsariosPreenchidaComSucesso() throws ParseException {
+    void shouldReturnAListOfAllUsersWithSuccess() throws ParseException {
         // Given
         List<Car> carros1 = new ArrayList<>();
         List<Car> carros2 = new ArrayList<>();
@@ -62,5 +70,77 @@ class UserServiceImplTest {
 
         // Then
         assertEquals(2, result.size(), "Deve conter dois UserDTO na lista.");
+    }
+
+    @Test
+    void shouldReturnAnUserIfEmailAndLoginValids() throws ParseException {
+        UserDTO userDTO = getUserDTO();
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(userRepository.findByLogin(anyString())).thenReturn(Optional.empty());
+
+        UserDTO createdUser = userService.createUser(userDTO);
+
+        assertNotNull(createdUser);
+        assertEquals("paulo.joao@email.com", createdUser.getEmail());
+    }
+
+    @Test
+    void shouldThrowEmailExceptionIfEmailAlreadyExists() throws ParseException {
+        UserDTO userDTO = getUserDTO();
+
+        User existingUser = new User();
+        existingUser.setEmail("paulo.joao@email.com");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(existingUser));
+
+        assertThrows(EmailException.class, () -> {
+            userService.createUser(userDTO);
+        });
+    }
+
+    @Test
+    void shouldThrowLoginExceptionIfLoginAlreadyExists() throws ParseException {
+        UserDTO userDTO = getUserDTO();
+
+        User existingUser = new User();
+        existingUser.setLogin("jp.silva");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(userRepository.findByLogin(anyString())).thenReturn(Optional.of(existingUser));
+
+        assertThrows(LoginException.class, () -> {
+            userService.createUser(userDTO);
+        });
+    }
+
+    @Test
+    void shouldThrowRuntimeException() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setEmail("valid@example.com");
+        userDTO.setLogin("validUser");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(userRepository.findByLogin(anyString())).thenReturn(Optional.empty());
+        doThrow(new RuntimeException("Database error")).when(userRepository).save(any());
+
+        assertThrows(RuntimeException.class, () -> {
+            userService.createUser(userDTO);
+        });
+    }
+
+    private static UserDTO getUserDTO() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthDay = dateFormat.parse("1980-01-20");
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(null);
+        userDTO.setFirstName("João");
+        userDTO.setLastName("Paulo da Silva");
+        userDTO.setBirthday(birthDay);
+        userDTO.setEmail("paulo.joao@email.com");
+        userDTO.setLogin("jp.silva");
+        userDTO.setPassword("15464654");
+        userDTO.setPhone("819999-9999");
+        return userDTO;
     }
 }
