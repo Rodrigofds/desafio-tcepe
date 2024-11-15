475ff714ad06d9d43fd8db0f5795e494dd02da53
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2024-11-14 21:56:15 -0300

    [USER006] feature - Implementa endpoint para listar todos os carros dos usuário.

---
+++ src/main/java/com/pitang/desafio/tcepe/service/ICarService.java
@@ -0,0 +1,11 @@
+package com.pitang.desafio.tcepe.service;
+
+import com.pitang.desafio.tcepe.dto.CarDTO;
+
+import java.util.List;
+
+public interface ICarService {
+
+    List<CarDTO> findAllCars();
+
+}
